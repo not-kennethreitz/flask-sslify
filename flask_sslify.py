9c12df10c408e2a644a7b729cf36f7dda26ccccf
Jeffrey Gelens <jeffrey@Icarus.local>
2013-07-18 09:43:09 +0200

    Fixed bug when initializing SSLify without 'app' argument and using init_app(app) to set it later.

--- flask_sslify.py
+++ flask_sslify.py
@@ -8,7 +8,7 @@ YEAR_IN_SECS = 31536000
 class SSLify(object):
     """Secures your Flask App."""
 
-    def __init__(self, app, age=YEAR_IN_SECS, subdomains=False, permanent=False):
+    def __init__(self, app=None, age=YEAR_IN_SECS, subdomains=False, permanent=False):
         if app is not None:
             self.app = app
             self.hsts_age = age
@@ -21,6 +21,8 @@ class SSLify(object):
 
     def init_app(self, app):
         """Configures the configured Flask app to enforce SSL."""
+
+        self.app = app
         app.before_request(self.redirect_to_ssl)
         app.after_request(self.set_hsts_header)
 
